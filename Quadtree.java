8a0d75a95329ca0a013b726cebd1119c6c8a97fc
Théotime Godefroy <theotime.godefroy@etu.univ-nantes.fr>
2023-11-13 19:01:29 +0100

    Updates Lundi après cours + dimanche oublies

--- Quadtree.java
+++ Quadtree.java
@@ -49,7 +49,8 @@ class Quadtree {
             this.Q2 = null;
             this.Q3 = null;
             this.Q4 = null;
-            creerQuad(this, tabQ);
+            this.creerQuad(tabQ);
+            this.verifEqui();
 
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -57,18 +58,16 @@ class Quadtree {
         }
     }
 
-    public void creerQuad(Quadtree Q, int[][] tabVal) {
+    public void creerQuad(int[][] tabVal) {
         if (tabVal.length == 2) {
-
             Quadtree q1 = new Quadtree(tabVal[0][0], null, null, null, null);
             Quadtree q2 = new Quadtree(tabVal[0][1], null, null, null, null);
             Quadtree q3 = new Quadtree(tabVal[1][1], null, null, null, null);
             Quadtree q4 = new Quadtree(tabVal[1][0], null, null, null, null);
-            Q.Q1 = q1;
-            Q.Q2 = q2;
-            Q.Q3 = q3;
-            Q.Q4 = q4;
-
+            this.Q1 = q1;
+            this.Q2 = q2;
+            this.Q3 = q3;
+            this.Q4 = q4;
         } 
         else {
             int tailleSousTableau = tabVal.length / 2;
@@ -76,7 +75,6 @@ class Quadtree {
             int[][] sousTableau2 = new int[tailleSousTableau][tailleSousTableau];
             int[][] sousTableau3 = new int[tailleSousTableau][tailleSousTableau];
             int[][] sousTableau4 = new int[tailleSousTableau][tailleSousTableau];
-
             for (int i = 0; i < tailleSousTableau; i++) {
                 for (int j = 0; j < tailleSousTableau; j++) {
                     sousTableau1[i][j] = tabVal[i][j];
@@ -85,19 +83,45 @@ class Quadtree {
                     sousTableau4[i][j] = tabVal[i + tailleSousTableau][j + tailleSousTableau];
                 }
             }
-
             Quadtree NQ1 = new Quadtree();
             Quadtree NQ2 = new Quadtree();
             Quadtree NQ3 = new Quadtree();
             Quadtree NQ4 = new Quadtree();
-            creerQuad(NQ1, sousTableau1);
-            creerQuad(NQ2, sousTableau2);
-            creerQuad(NQ3, sousTableau3);
-            creerQuad(NQ4, sousTableau4);
-            Q.Q1 = NQ1;
-            Q.Q2 = NQ2;
-            Q.Q3 = NQ3;
-            Q.Q4 = NQ4;
+            NQ1.creerQuad(sousTableau1);
+            NQ2.creerQuad(sousTableau2);
+            NQ3.creerQuad(sousTableau3);
+            NQ4.creerQuad(sousTableau4);
+            this.Q1 = NQ1;
+            this.Q2 = NQ2;
+            this.Q3 = NQ3;
+            this.Q4 = NQ4;
+        }
+    }
+
+    public void verifEqui(){
+        if (this.getQ1().getValue() == this.getQ2().getValue() 
+        && this.getQ2().getValue() == this.getQ3().getValue()
+        && this.getQ3().getValue() == this.getQ4().getValue()
+        && this.getQ1().getValue() != -1){
+            this.value = this.getQ1().getValue();
+            this.Q1=null;
+            this.Q2=null;
+            this.Q3=null;
+            this.Q4=null;
+        }
+        else{
+            if(this.getQ1().value == -1){
+                this.Q1.verifEqui();
+            }
+            if(this.getQ2().value == -1){
+                this.Q2.verifEqui();
+            }
+            if(this.getQ3().value == -1){
+                this.Q3.verifEqui();
+            }
+            if(this.getQ4().value == -1){
+                this.Q4.verifEqui();
+            }
         }
     }
 
@@ -125,8 +149,52 @@ class Quadtree {
         return this.value = v;
     }
 
-    public void toPGM() {
+    public int hauteurMax(){
+        if(this.getQ1() == null
+        &&this.getQ2() == null
+        &&this.getQ3() == null
+        &&this.getQ4() == null){
+            return 0;
+        }
+        else{
+            int maxQ1 = 1+this.getQ1().hauteurMax();
+            int maxQ2 = 1+this.getQ2().hauteurMax();
+            int maxQ3 = 1+this.getQ3().hauteurMax();
+            int maxQ4 = 1+this.getQ4().hauteurMax();
+            return Math.max(Math.max(maxQ1,maxQ2),Math.max(maxQ3,maxQ4));
+        }
+    }
 
+    public long lumMax(){
+        if(this.getQ1() == null
+        &&this.getQ2() == null
+        &&this.getQ3() == null
+        &&this.getQ4() == null){
+            return this.value;
+        }
+        else{
+            long maxQ1 = this.getQ1().lumMax();
+            long maxQ2 = this.getQ2().lumMax();
+            long maxQ3 = this.getQ3().lumMax();
+            long maxQ4 = this.getQ4().lumMax();
+            return Math.max(Math.max(maxQ1,maxQ2),Math.max(maxQ3,maxQ4));
+        }
+    }
+
+    public void toPGM() {
+        String fichier = "dessin.pgm";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fichier))){
+            writer.write("P2");
+            writer.newLine();
+            writer.write("# commentaire photo");
+            writer.newLine();
+            int dim = (int)Math.pow(2,this.hauteurMax());
+            writer.write(dim + " " + dim);
+            writer.newLine();
+            writer.write((int)this.lumMax());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void compressLambda() {
@@ -134,8 +202,8 @@ class Quadtree {
         &&this.getQ2().value != -1
         &&this.getQ3().value != -1
         &&this.getQ4().value != -1){
-            this.value = Math.round(Math.exp((1/4)*(Math.log(0.1+this.getQ1().value)) + (Math.log(0.1+this.getQ2().value))
-             + (Math.log(0.1+this.getQ3().value)) + (Math.log(0.1+this.getQ4().value))));
+            this.value = Math.round(Math.exp(0.25*(Math.log(0.1+this.getQ1().value) + Math.log(0.1+this.getQ2().value)
+             + Math.log(0.1+this.getQ3().value) + Math.log(0.1+this.getQ4().value))));
             this.Q1=null;
             this.Q2=null;
             this.Q3=null;
@@ -194,19 +262,19 @@ class Quadtree {
     }
 
     public static void main(String[] args) {
-        Quadtree Q = new Quadtree("test.pgm");
-        /*
-         * Quadtree Q1 = new Quadtree(1,null,null,null,null);
-         * Quadtree Q2 = new Quadtree(2,null,null,null,null);
-         * Quadtree Q3 = new Quadtree(3,null,null,null,null);
-         * Quadtree Q4 = new Quadtree(4,null,null,null,null);
-         * Quadtree Q0 = new Quadtree(0, Q1, Q2, Q3, Q4);
-         * Q0.QtoString();
-         */
+        Quadtree Q = new Quadtree("flower_small.pgm");
         Q.QtoString();
+        System.out.print("\nHauteur de larbre : ");
+        System.out.println(Q.hauteurMax());
+        System.out.print("Luminosité maximum d'un pixel de larbre : ");
+        System.out.println(Q.lumMax());
         Q.compressLambda();
-        System.out.println("");
-        System.out.println("Compression Lambda : ");
+        Q.verifEqui();
+        System.out.println("\nCompression Lambda : ");
         Q.QtoString();
+        System.out.print("\nHauteur de larbre : ");
+        System.out.println(Q.hauteurMax());
+        System.out.print("Luminosité maximum d'un pixel de l'arbre : ");
+        System.out.println(Q.lumMax());
     }
 }
